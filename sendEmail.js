d2f97430662dbedade4c9748b1b17960561cceb2
Brian Hsu <brianhsu305@gmail.com>
2021-04-15 18:30:38 -0700

    contact form set up

---
+++ sendEmail.js
@@ -0,0 +1,13 @@
+const send = document.getElementById('contact-form');
+
+send.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    emailjs.sendForm('gmail', 'portfolio_template', e.target,'user_C90iwOTsZIXz1u6iQBppj')
+        .then(() => {
+            alert('email has been sent!');
+        }, (error) => {
+            console.log('EMAIL FAILED SENDING...', error);
+    });
+    e.target.reset();
+})
