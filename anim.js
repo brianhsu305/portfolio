08116fffb6ae0d9c532743b1f70de56a8b9422d5
Brian Hsu <brianhsu305@gmail.com>
2021-04-19 18:02:00 -0700

    new project image 2

--- anim.js
+++ anim.js
@@ -57,7 +57,7 @@ $('.navbar a').on('click', function(e) {
         const hash = this.hash;
 
         $('html, body').animate({
-            scrollTop: $(hash).offset().top - 80
+            scrollTop: $(hash).offset().top - 50
         },800);
     }
 })
