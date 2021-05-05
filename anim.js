bbc570101dbbab4880e97bc1e09330af2139ca5f
Brian Hsu <brianhsu305@gmail.com>
2021-05-04 22:21:35 -0700

    transition2

--- anim.js
+++ anim.js
@@ -1,63 +1,54 @@
 gsap.registerPlugin(ScrollTrigger);
 tl = gsap.timeline();
 
-tl.from('.stagger1', {
-    y: '-30%',
-    opacity: 0,
-    duration: 3,
-    stagger: .3,
-    ease: Power4.easeOut  
-})
-
-
-
-gsap.from('.transition2', {
-    scrollTrigger: {
-        trigger: '.transition2',
-        start: 'top bottom',
-        toggleActions: "restart",
-    },
-    opacity: 0,
-    y: 50,
-    duration: 1.2,
-    stagger: .3
-})
-
-gsap.from('.transition3', {
-    scrollTrigger: {
-        trigger: '.transition3',
-        start: 'top bottom',
-        toggleActions: "restart",
-    },
-    opacity: 0,
-    x: -50,
-    duration: 1.2,
-    stagger: .3
-})
-
-gsap.from('.transition4', {
-    scrollTrigger: {
-        trigger: '.transition4',
-        start: 'top bottom',
-        toggleActions: "restart",
-    },
-    opacity: 0,
-    scale: .1,
-    ease: Power3.easeOut,
-    duration: 2,
-    stagger: .3
-})
-
+tl.from(".stagger1", { y: "-30%", duration: 3, opacity: 0, stagger: 0.3, ease: Power4.easeOut });
+gsap.from(".transition2", {
+	scrollTrigger: {
+		trigger: ".transition2",
+		start: "top bottom",
+		toggleActions: "restart",
+	},
+	opacity: 0,
+	y: 50,
+	duration: 1.2,
+	stagger: 0.3,
+});
+gsap.from(".transition3", {
+	scrollTrigger: {
+		trigger: ".transition3",
+		start: "top bottom",
+		toggleActions: "restart",
+	},
+	opacity: 0,
+	x: -50,
+	duration: 1.2,
+	stagger: 0.3,
+});
+gsap.from(".transition4", {
+	scrollTrigger: {
+		trigger: ".transition4",
+		start: "top bottom",
+		toggleActions: "restart",
+	},
+	opacity: 0,
+	scale: 0.1,
+	ease: Power3.easeOut,
+	duration: 2,
+	stagger: 0.3,
+});
 
 //navbar smooth scrolling using jquery
-$('.navbar a').on('click', function(e) {
-    if(this.hash !== '') {
-        e.preventDefault();
+$(".navbar a").on("click", function (e) {
+	if (this.hash !== "") {
+		e.preventDefault();
 
-        const hash = this.hash;
+		const hash = this.hash;
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top - 60
-        },800);
-    }
-})
+		$("html, body").animate(
+			{
+				scrollTop: $(hash).offset().top - 60,
+			},
+			800,
+		);
+	}
+});
